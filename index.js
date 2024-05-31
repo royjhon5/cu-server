350b77561401d2a62bc8a2ed90da648407c0812d
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2024-05-31 16:26:07 +0800

    update: latest update test

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
-const PORT = 8000
+const http = require('http').Server(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('user_profile_picture'))
 app.use(cookieParser()); 
 app.use(cors({
-  origin: (origin, callback) => {
+    origin: (origin, callback) => {
       const allowedOrigins = ["https://cu-app-admin.vercel.app", "https://cu-app-giftshop.vercel.app"];
-      if (allowedOrigins.includes(origin) || !origin) {
-          callback(null, true);
-      } else {
-          callback(new Error('Not allowed by CORS'));
-      }
-  },
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  credentials: true,
-  optionsSuccessStatus: 204
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+    optionsSuccessStatus: 204
 }));
 
 app.get('/', (req, res) => {
@@ -28,7 +27,4 @@ app.get('/', (req, res) => {
 });
 
 require('./routes/routerManager')(app);
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
-module.exports = app
+http.listen(process.env.PORT || 8000);
